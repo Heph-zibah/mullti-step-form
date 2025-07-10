87089ac62d9257ba5d13fdcccd3f7b2bc5d831f6
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-07-10 21:45:55 +0100

    worked on the footer and readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
     <div className="">
-      <footer className="text-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.frontendmentor.io/profile/Heph-zibah"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tosin Daramola
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-center">
+      <p className="text-sm text-gray-500 dark:text-gray-400">
+        Created by{" "}
+        <a
+          className=""
+          href="https://www.frontendmentor.io/profile/Heph-zibah"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tosin Daramola
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer
