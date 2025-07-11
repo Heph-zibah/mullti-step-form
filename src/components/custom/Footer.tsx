91fda1f74a84b04db7b59b6526e0947a8259d64d
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-07-11 13:31:10 +0100

    worked on the footer

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="text-center">
+    <footer className="text-center my-3">
       <p className="text-sm text-gray-500 dark:text-gray-400">
         Created by{" "}
         <a
-          className=""
+          className="text-[var(--blue-950)] font-bold hover:underline"
           href="https://www.frontendmentor.io/profile/Heph-zibah"
           target="_blank"
           rel="noopener noreferrer"
